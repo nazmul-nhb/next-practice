9873e9556b2d62b2b4acb3d2c2f09e448bb55002
Nazmul Hassan <nazmulnhb007@yahoo.com>
2024-10-18 23:27:38 +0600

    created basic routes and navlinks

---
+++ app/exit/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ExitPage = () => {
+    return (
+        <div>
+            This is Exit Page
+        </div>
+    );
+};
+
+export default ExitPage;
--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
 	return (
-		<main className="flex items-center justify-center w-full h-screen">
-      Hello Next.js
-    </main>
+		<main>
+			Hello Next.js
+		</main>
 	);
 }
