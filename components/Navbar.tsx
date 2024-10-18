a6cb3a1542885590aa8aedcfe82561551be63528
Nazmul Hassan <nazmulnhb007@yahoo.com>
2024-10-19 00:22:44 +0600

    conditionally style navlinks; used useRouter and usePathName hooks from next navigation

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,12 +1,39 @@
+"use client";
+
 import React from "react";
 import Link from "next/link";
+import { usePathname, useRouter } from "next/navigation";
 
-const Navbar:React.FC = () => {
+const Navbar: React.FC = () => {
+	const pathName = usePathname();
+	const router = useRouter();
+
+	const navLinks = [
+		{ path: "/", title: "Home" },
+		{ path: "/about", title: "About" },
+		{ path: "/exit", title: "Exit" },
+	];
+
+	const handleLogout = () => {
+		router.push("/exit");
+	};
+
 	return (
 		<nav className="h-[10vh] flex gap-6 items-center justify-around">
-			<Link href={"/"}>Home</Link>
-			<Link href={"/about"}>About</Link>
-			<Link href={"/exit"}>Exit</Link>
+			{navLinks.map((link) => (
+				<Link
+					className={
+						pathName === link.path
+							? "pb-1 border-b border-white"
+							: "pb-1 border-b border-transparent"
+					}
+					key={link.path}
+					href={link.path}
+				>
+					{link.title}
+				</Link>
+			))}
+			<button onClick={handleLogout}>Logout</button>
 		</nav>
 	);
 };
