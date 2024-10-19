23338260cb113e3211790e8c64d220c57ef020b5
Nazmul Hassan <nazmulnhb007@yahoo.com>
2024-10-20 01:50:56 +0600

    fetch data using axios

--- app/products/page.tsx
+++ app/products/page.tsx
@@ -4,15 +4,22 @@ import Image from "next/image";
 import Link from "next/link";
 
 const ProductsPage = async () => {
-	const { totalProducts, products } = await getProducts()
-	console.log(totalProducts);
+	const { totalProducts = 0, products = [] } = await getProducts();
+
 	return (
 		<section className="grid gap-5 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
-			{products.map((product) => {
+			<h3 className="col-span-1 md:col-span-2 lg:col-span-3 xl:col-span-4 text-xl font-semibold text-center">
+				Total {totalProducts} Products
+			</h3>
+			{products?.slice(0, 40).map((product) => {
 				const { _id, productImage, title } = product;
 
 				return (
-					<Link className="hover:scale-105 transition-all duration-500 m-3 border p-1" href={`products/${_id}`} key={_id}>
+					<Link
+						className="hover:scale-105 transition-all duration-500 m-3 border p-1"
+						href={`products/${_id}`}
+						key={_id}
+					>
 						<h3>{title}</h3>
 						<Image
 							width={480}
--- utilities/fetchData.ts
+++ utilities/fetchData.ts
@@ -1,16 +1,28 @@
 import { IProductResponse, IProductsResponse } from "@/types/interfaces";
+import axios from "axios";
 
 export const productUrl =
 	"https://rtk-product-management-server.vercel.app/products";
 
+// export const getProducts = async (): Promise<IProductsResponse> => {
+// 	const response = await fetch(productUrl);
+// 	const data: IProductsResponse = await response.json();
+// 	return data;
+// };
+
+// export const getProductById = async (id: string): Promise<IProductResponse> => {
+// 	const response = await fetch(`${productUrl}/${id}`);
+// 	const data: IProductResponse = await response.json();
+// 	return data;
+// };
+
+// Using Axios
 export const getProducts = async (): Promise<IProductsResponse> => {
-	const response = await fetch(productUrl);
-	const data: IProductsResponse = await response.json();
+	const { data } = await axios.get<IProductsResponse>(productUrl);
 	return data;
 };
 
 export const getProductById = async (id: string): Promise<IProductResponse> => {
-	const response = await fetch(`${productUrl}/${id}`);
-	const data: IProductResponse = await response.json();
+	const { data } = await axios.get<IProductResponse>(`${productUrl}/${id}`);
 	return data;
 };
